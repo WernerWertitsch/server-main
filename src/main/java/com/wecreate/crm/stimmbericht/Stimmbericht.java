d8cd7d8ce11bf81966711ab99c4e116d13585ca3
BHS <Bernd.Steurer@gmx.at>
2020-04-20 18:42:56 +0200

    +Stimmbericht

---
+++ src/main/java/com/wecreate/crm/stimmbericht/Stimmbericht.java
@@ -0,0 +1,28 @@
+package com.wecreate.crm.stimmbericht;
+
+public class Stimmbericht {
+
+    String id;
+    String clientid;
+    String luftfeuchtigkeit;
+    String klavierstimmung_vorher;
+    String klavierstimmung_nachher;
+    String resonanzboden;
+    String stege;
+    String saiten;
+    String stimmwirbel;
+    String stimmstock;
+    String hammerkoepfe;
+    String mechanik;
+    String daempfer;
+    String tastatur;
+    String oberflaeche;
+    String rahmen;
+    String anmerkungen;
+    String oertlichkeit;
+    String transport;
+    String nacharbeit;
+    String zahlungsart;
+    Boolean kostenvoranschlag;
+
+}
